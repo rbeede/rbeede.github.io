67b749c760fdc882b45de494a5138be7de1aa2ff
Rodney Beede <rbeede@users.noreply.github.com>
2018-11-29 15:12:55 -0600

    Added more non-interactive flags

--- downloads/ubuntu-update-and-reboot.bash
+++ downloads/ubuntu-update-and-reboot.bash
@@ -17,15 +17,18 @@ exec 2>&1
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 
-/usr/bin/apt-get --quiet --assume-yes update;
+export DEBIAN_FRONTEND=noninteractive
 
-/usr/bin/apt-get --quiet --assume-yes -o Dpkg::Options::='--force-confold' dist-upgrade;
 
-/usr/bin/apt-get --quiet --assume-yes autoremove;
+/usr/bin/apt-get --quiet --assume-yes update
 
-/usr/bin/apt-get --quiet --assume-yes autoclean;
+/usr/bin/apt-get --quiet --assume-yes -o 'Dpkg::Options::=--force-confdef' -o 'Dpkg::Options::=--force-confold' dist-upgrade
 
-/usr/bin/apt-get --quiet --assume-yes clean;
+/usr/bin/apt-get --quiet --assume-yes autoremove
+
+/usr/bin/apt-get --quiet --assume-yes autoclean
+
+/usr/bin/apt-get --quiet --assume-yes clean
 
 
 if [ -f /var/run/reboot-required ]; then
